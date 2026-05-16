1c399662a4f1c28f9944b039b651909ad7eb744d
Nar PJ <putchakarnj.s9@gmail.com>
2026-05-16 16:12:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -497,7 +497,7 @@ if uploaded_file:
                     figsize=(13, max(2.6, 0.7 * n_issues))
                 )
                 bars = ax_iss.barh(
-                    plot_df["Issues"].apply(categorize_issue),
+                    plot_df["Issues": issues_label].apply(categorize_issue),
                     plot_df["% of Total"],
                     color=plot_colors,
                 )
